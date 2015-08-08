2bde9ba1d4a52f01f91fd035e390606f3dbd65d1
Jorge Alegre <git@jorgealegre.com>
2015-08-08 12:58:38 +0200

    slug changes to group document mapping

--- ximpia_api/ximpia_api/apps/user/management/commands/setup_site.py
+++ ximpia_api/ximpia_api/apps/user/management/commands/setup_site.py
@@ -2,11 +2,23 @@ from django.core.management.base import BaseCommand
 
 __author__ = 'jorgealegre'
 
+"""
+
+Command or web access???
+
+Ideal world....
+
+1. User connects to http://api_host/setup
+2. Connects to Twitter, Facebook, Gmail
+3. Name, optional username: Would create user attached to admin group
+
+"""
+
 
 class Command(BaseCommand):
     help = ''
     can_import_settings = True
 
     def handle(self, *args, **options):
-        # 1. Input user info about admin
+        # 1. Input user info about admin: username??? password
         pass
