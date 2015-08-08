e7d905b7497321c08682e943e0354b5a78253262
Jorge Alegre <git@jorgealegre.com>
2015-08-08 13:55:09 +0200

    setup site command, which calls API endpoint to setup site, creating indices, admin user and groups

--- ximpia_api/ximpia_api/apps/user/management/commands/setup_site.py
+++ ximpia_api/ximpia_api/apps/user/management/commands/setup_site.py
@@ -1,27 +1,14 @@
 from django.core.management.base import BaseCommand
+# import requests
 
 __author__ = 'jorgealegre'
 
-"""
-
-Command or web access???
-
-Ideal world....
-
-1. User connects to http://api_host/setup
-
-or https://{site}.ximpia.com/
-
-2. Connects to Twitter, Facebook, Gmail: Using JS and social network buttons
-3. Site name / Project name
-
-"""
-
 
 class Command(BaseCommand):
-    help = ''
+    help = 'System command to setup site'
     can_import_settings = True
 
     def handle(self, *args, **options):
-        # 1. Input user info about admin: username??? password
-        pass
+        name = raw_input(u'Your Name')
+        # Call API and POST data to setup site
+        # Created indices, create groups staff and admin, add permissions is_admin and is_staff to user permissions
