9466839d02fe5d8e1a6dcf960cd6f2608df34cd1
Jorge Alegre <git@jorgealegre.com>
2015-08-08 13:44:51 +0200

    permissions to group

--- ximpia_api/ximpia_api/apps/user/management/commands/setup_site.py
+++ ximpia_api/ximpia_api/apps/user/management/commands/setup_site.py
@@ -9,8 +9,11 @@ Command or web access???
 Ideal world....
 
 1. User connects to http://api_host/setup
-2. Connects to Twitter, Facebook, Gmail
-3. Name, optional username: Would create user attached to admin group
+
+or https://{site}.ximpia.com/
+
+2. Connects to Twitter, Facebook, Gmail: Using JS and social network buttons
+3. Site name / Project name
 
 """
 
