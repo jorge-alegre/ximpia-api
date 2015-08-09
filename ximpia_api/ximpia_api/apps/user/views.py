df43708bcf2ee407b8e1c119ca21dd2646b89bf7
Jorge Alegre <git@jorgealegre.com>
2015-08-09 17:24:29 +0200

    autocomplete fields for user

--- ximpia_api/ximpia_api/apps/user/views.py
+++ ximpia_api/ximpia_api/apps/user/views.py
@@ -7,9 +7,12 @@ Routes
 We would define this from front, being some map simple to understand.
 
 /contributions/{contribution_slug:slug_type}/
+
+above is the one to be configured, then we can access with:
+
 /contributions/{contribution_slug:slug_type}/create
 /contributions/{contribution_slug:slug_type}/update
-/contributions/{contribution_slug:slug_type}/list (these would accept Ximpia queries)
+/contributions/{contribution_slug:slug_type}/list (these would accept Ximpia queries, input, query_name)
 /contributions/{contribution_slug:slug_type}/detail
 
 """
