278e0297d9f32396d20dc84e8ff04549f3bb8953
Jorge Alegre <git@jorgealegre.com>
2015-08-09 17:16:54 +0200

    link apps to permissions

--- ximpia_api/ximpia_api/apps/user/views.py
+++ ximpia_api/ximpia_api/apps/user/views.py
@@ -1,1 +1,16 @@
 __author__ = 'jorgealegre'
+
+"""
+Routes
+======
+
+We would define this from front, being some map simple to understand.
+
+/contributions/{contribution_slug:slug_type}/
+/contributions/{contribution_slug:slug_type}/create
+/contributions/{contribution_slug:slug_type}/update
+/contributions/{contribution_slug:slug_type}/list (these would accept Ximpia queries)
+/contributions/{contribution_slug:slug_type}/detail
+
+"""
+
