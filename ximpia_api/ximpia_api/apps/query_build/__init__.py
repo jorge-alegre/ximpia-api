4f7919c89b20f92d2f450b5dae3405af32347d33
Jorge Alegre <git@jorgealegre.com>
2015-09-16 00:45:11 +0200

    filters and excludes in queries

--- ximpia_api/ximpia_api/apps/query_build/__init__.py
+++ ximpia_api/ximpia_api/apps/query_build/__init__.py
@@ -10,15 +10,15 @@ def get_query_request(payload):
       "query": "cat",
       "filters": {
         "must": {
-           name: value
-        },
-        "should": [
-           {
-             "field": "",
-             "value": "",
-             "rating": 5
+           name: value,
+           name: {
+             "gte": 23,
+             "lte": 90
            }
-        ]
+        },
+        "should": {
+          name: value
+        }
       },
       "excludes": {
         "user.id": 34
@@ -51,6 +51,10 @@ def get_query_request(payload):
         ]
       }
     }
+
+    Ranges:
+    gte:2015-11-01
+    gt:2015-11-01
 
     :param payload: Dictionary with payload
     :return: ElasticSearch DSL Query as dictionary
@@ -95,21 +99,62 @@ def get_query_request(payload):
     # filters
     if 'filters' in payload and 'must' in payload['filters'] and isinstance(payload['filters']['must'], dict):
         query_dsl['query']['filtered'].setdefault('filters', {})
-        query_dsl['query']['filtered']['filters']['must'] = map(lambda x: {
-            'term': {
-                x[0]: x[1]
-            }
-        },
-            payload['filters']['must'].items())
+        query_dsl['query']['filtered']['filters'].setdefault('bool', {})
+        # We should have ranges as well
+        must_list = []
+        for field, value in payload['filters']['must'].items():
+            if isinstance(value, dict) and ('gte' in value.keys() or 'gt' in value.keys() or 'lte' in value.keys()
+                                            or 'lt' in value.keys()):
+                must_list.append(
+                    {
+                        "range": {
+                            field: value
+                        }
+                    }
+                )
+            else:
+                must_list.append(
+                    {
+                        "term": {
+                            field: value
+                        }
+                    }
+                )
+        query_dsl['query']['filtered']['filters']['bool']['must'] = must_list
     elif 'filters' in payload and 'must' in payload['filters'] and isinstance(payload['filters']['should'], dict):
         query_dsl['query']['filtered'].setdefault('filters', {})
-        query_dsl['query']['filtered']['filters']['should'] = map(lambda x: {
-            'term': {
-                x[0]: x[1]
-            }
-        },
-            payload['filters']['should'].items())
+        query_dsl['query']['filtered']['filters'].setdefault('bool', {})
+        should_list = []
+        for field, value in payload['filters']['should'].items():
+            if isinstance(value, dict) and ('gte' in value.keys() or 'gt' in value.keys() or 'lte' in value.keys()
+                                            or 'lt' in value.keys()):
+                should_list.append(
+                    {
+                        "range": {
+                            field: value
+                        }
+                    }
+                )
+            else:
+                should_list.append(
+                    {
+                        "term": {
+                            field: value
+                        }
+                    }
+                )
+        query_dsl['query']['filtered']['filters']['bool']['should'] = should_list
     # excludes
+    if 'excludes' in payload:
+        query_dsl['query']['filtered']['filters']['not'] = {
+            "bool": {
+                "must": map(lambda x: {
+                    "term": {
+                        x[1]: x[2]
+                    }
+                }, payload['excludes'].items())
+            }
+        }
     # sort
-    # group_by
+    # group_by: term aggregation
     return query_dsl
