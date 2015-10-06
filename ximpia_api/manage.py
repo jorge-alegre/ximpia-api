5e19dfbd69de448869fd2b3aca0c43d0d3e4376f
Jorge Alegre <git@jorgealegre.com>
2015-10-06 22:15:18 +0200

    path temp insert into manage

--- ximpia_api/manage.py
+++ ximpia_api/manage.py
@@ -2,6 +2,9 @@
 import os
 import sys
 
+sys.path.append('{}/ximpia_api/apps/'.format(os.getcwd()))
+sys.path.append('ximpia_api')
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ximpia_api.settings")
 
