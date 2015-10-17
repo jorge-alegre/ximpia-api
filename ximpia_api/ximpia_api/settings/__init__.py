3356f8840404da7462c374388161b386b6165ad3
Jorge Alegre <git@jorgealegre.com>
2015-10-17 13:50:22 +0200

    output fix path

--- ximpia_api/ximpia_api/settings/__init__.py
+++ ximpia_api/ximpia_api/settings/__init__.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '../'
 
 sys.path.append('{}/ximpia_api/apps/'.format(os.getcwd()))
 sys.path.append('{}/ximpia_api/'.format(os.getcwd()))
-
+print sys.path
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
