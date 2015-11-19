fddd9be51f409e0c155e2c29892e908333235b42
Jorge Alegre <git@jorgealegre.com>
2015-11-19 23:41:09 +0100

    command parameters for create users

--- ximpia_api/ximpia_api/apps/base/management/commands/create_fb_test_users.py
+++ ximpia_api/ximpia_api/apps/base/management/commands/create_fb_test_users.py
@@ -1,5 +1,6 @@
 import logging
 import json
+from optparse import make_option
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
@@ -15,6 +16,19 @@ class Command(BaseCommand):
     help = 'Create logged in test users for facebook with persistence'
     can_import_settings = True
 
+    option_list = BaseCommand.option_list + (
+        make_option('--feature',
+                    action='store',
+                    dest='feature',
+                    default='base',
+                    help='Feature'),
+        make_option('--size',
+                    action='store',
+                    dest='size',
+                    default=5,
+                    help='Number users'),
+    )
+
     def handle(self, *args, **options):
         """
         Create fb test users, make action to login for them.
@@ -25,11 +39,11 @@ class Command(BaseCommand):
         :param options:
         :return:
         """
-        feature = options.get('feature', 'base')
-        size = options.get('size', 5)
+        feature = options['feature']
+        size = options['size']
         # Create and log in fb users
-        with open('{}apps/base/tests/data/fb_test_users.json'.format(settings.BASE_DICT)) as f:
+        with open('{}/apps/base/tests/data/fb_test_users.json'.format(settings.BASE_DIR)) as f:
             users = json.loads(f.read())
-        for user_counter in range(size):
-            user_data = create_fb_test_user_login()
-            users[feature].append(user_data)
+            for user_counter in range(size):
+                user_data = create_fb_test_user_login()
+                users[feature].append(user_data)
