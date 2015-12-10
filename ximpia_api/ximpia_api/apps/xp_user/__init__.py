dc200bc21b934165927a1075a0e4a38b71092c2b
Jorge Alegre <git@jorgealegre.com>
2015-12-10 01:01:31 +0100

    login and logout

--- ximpia_api/ximpia_api/apps/xp_user/__init__.py
+++ ximpia_api/ximpia_api/apps/xp_user/__init__.py
@@ -1,1 +1,89 @@
+from django.conf import settings
+from django.apps import apps as django_apps
+from django.core.exceptions import ImproperlyConfigured
+from django.middleware.csrf import rotate_token
+from django.utils.translation import LANGUAGE_SESSION_KEY
+
 __author__ = 'jorgealegre'
+
+
+SESSION_KEY = '_auth_user_id'
+BACKEND_SESSION_KEY = '_auth_user_backend'
+HASH_SESSION_KEY = '_auth_user_hash'
+REDIRECT_FIELD_NAME = 'next'
+
+
+def get_user_model():
+    """
+    Returns the User model that is active in this project.
+    """
+    try:
+        return django_apps.get_model(settings.AUTH_USER_MODEL)
+    except ValueError:
+        raise ImproperlyConfigured("AUTH_USER_MODEL must be of the form 'app_label.model_name'")
+    except LookupError:
+        raise ImproperlyConfigured(
+            "AUTH_USER_MODEL refers to model '%s' that has not been installed" % settings.AUTH_USER_MODEL
+        )
+
+
+def _get_user_session_key(request):
+    # This value in the session is always serialized to a string, so we need
+    # to convert it back to Python whenever we access it.
+    return get_user_model()._meta.pk.to_python(request.session[SESSION_KEY])
+
+
+def login(request, user):
+    """
+    Persist a user id and a backend in the request. This way a user doesn't
+    have to reauthenticate on every request. Note that data set during
+    the anonymous session is retained when the user logs in.
+    """
+    session_auth_hash = ''
+    if user is None:
+        user = request.user
+    if hasattr(user, 'get_session_auth_hash'):
+        session_auth_hash = user.get_session_auth_hash()
+
+    if SESSION_KEY in request.session:
+        """if _get_user_session_key(request) != user.pk or (
+                session_auth_hash and
+                request.session.get(HASH_SESSION_KEY) != session_auth_hash):
+            # To avoid reusing another user's session, create a new, empty
+            # session if the existing session corresponds to a different
+            # authenticated user."""
+        request.session.flush()
+    else:
+        request.session.cycle_key()
+    request.session[SESSION_KEY] = user.pk
+    request.session[BACKEND_SESSION_KEY] = user.backend
+    request.session[HASH_SESSION_KEY] = session_auth_hash
+    if hasattr(request, 'user'):
+        request.user = user
+    rotate_token(request)
+    # user_logged_in.send(sender=user.__class__, request=request, user=user)
+
+
+def logout(request):
+    """
+    Removes the authenticated user's ID from the request and flushes their
+    session data.
+    """
+    # Dispatch the signal before the user is logged out so the receivers have a
+    # chance to find out *who* logged out.
+    user = getattr(request, 'user', None)
+    if hasattr(user, 'is_authenticated') and not user.is_authenticated():
+        user = None
+    # user_logged_out.send(sender=user.__class__, request=request, user=user)
+
+    # remember language choice saved to session
+    language = request.session.get(LANGUAGE_SESSION_KEY)
+
+    request.session.flush()
+
+    if language is not None:
+        request.session[LANGUAGE_SESSION_KEY] = language
+
+    if hasattr(request, 'user'):
+        from django.contrib.auth.models import AnonymousUser
+        request.user = AnonymousUser()
