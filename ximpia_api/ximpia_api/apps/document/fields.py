63be682386682c0bc602a15091639faa4b6e12a4
Jorge Alegre <git@jorgealegre.com>
2015-12-20 22:55:03 +0100

    exists and not exists basic patterns with bulk query support

---
+++ ximpia_api/ximpia_api/apps/document/fields.py
@@ -0,0 +1,199 @@
+
+from django.conf import settings
+from django.utils.translation import ugettext as _
+from django.utils.text import slugify
+
+from base import exceptions
+from document import Document
+
+__author__ = 'jorgealegre'
+
+
+u"""
+Info on how document definition will work
+
+Create Document Definition
+==========================
+
+This is simple example of just status with choices definition
+
+String Field
+------------
+
+Here we see need to reference other values, which is made in python code by using method, variable, etc...
+Would be good to have something generic. Need for choices, default values, etc...
+
+Here ref: is reserved symbol to make reference anywhere in system. Definition is on the target item. After ref:
+we have a path.
+
+hint, display_name and other fields that are international chars would go somewhere where guys can translate easily.
+Translation not defined in the document definition. We have a list of internationalized attribute names.
+
+{
+    'status': {
+        'type': 'string',
+        'choices': 'ref:choices.status',
+        'default': 'ref:choices.draft',
+        'hint': 'Content status for documents',
+        'display_name': 'Status',
+        'max_length': 100,
+        'min_length': 25
+    }
+}
+
+You can also define defaults relative to settings
+
+{
+    'place': {
+        'type': 'string',
+        'default': 'ref:settings.places.coffee_shop',
+    }
+}
+
+JSON structure bellow 'status' would be document field definition.
+
+field = StringField(**status_definition)
+
+This way we build document definition with all fields, and we can generate ES mappings for all fields
+in a document, check validations, etc...
+
+Validations:
+We would have a list of validation rules, that would be parsed by pattern definitions internally. First release
+would have limited set of validations, like exists, not-exists. Keep in mind that validation rules would be
+free type, parsed by the pattern classes. Idea is that we can build patterns on demand to apply new logic,
+business logic into create operations, etc...
+
+This would create in case document customer-code is unique.
+{
+    'customer-code': {
+        'type': 'string',
+        'validations': [
+            {
+                'type': 'not-exists',
+            }
+        ]
+    }
+}
+
+Above definition would generate a query to check that document has customer_code for field value.
+
+"""
+
+
+class StringField(object):
+
+    allowed_attributes = {
+        u'add_summary',
+        u'choices',
+        u'default',
+        u'hint',
+        u'comment',
+        u'display_name',
+        u'max_length',
+        u'min_length',
+    }
+
+    name = None
+    default = None
+    choices = None
+    add_to_summary = None
+    hint = None
+    comment = None
+    display_name = None
+    max_length = None
+    min_length = None
+    validations = None
+
+    def __init__(self, **kwargs):
+        """
+        Document contains a list of fields, each with configuration for validation, db checks,
+        and additional logic.
+
+        *Attributes*
+
+        * name
+        * add_summary
+        * choices
+        * default
+        * hint
+        * comment
+        * display_name
+
+        *Notes*
+
+        * Django like field instance in Document??? Dictionary type?
+        * Have in mind we will not use directly in code, like Django model
+        * We would create/update document_definition document with configuration, validations, etc...
+
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        not_validated_field = filter(lambda x: x not in self.allowed_attributes, kwargs)
+        if not_validated_field:
+            raise exceptions.XimpiaAPIException(_(u''))
+        for attr_name in kwargs:
+            setattr(self, attr_name, kwargs[attr_name])
+
+    def make_mapping(self):
+        """
+        Create the mapping structure to create mappings and update mappings
+
+        :return:
+        """
+        # We could set analyzer. Default analyzer??? International chars???
+        # How we set version fields??? We get field versions for name
+        # TODO: integrate version
+        version = ''
+        mappings = {
+            u'{}__{}'.format(
+                self.name, version
+            ): {
+                u'type': u'string',
+                u'fields': {
+                    u'{}__{}'.format(
+                        self.name, version): {
+                            u'type': u'string'
+                    },
+                    u'raw__{}'.format(version): {
+                        u'type': u'string',
+                        u'index': u'not_analyzed'
+                    }
+                }
+            }
+        }
+        if self.default:
+            mappings[u'null_value'] = self.default
+        if self.add_to_summary:
+            mappings[u'copy_to'] = u'text__v1'
+        return mappings
+
+    @classmethod
+    def validate(cls, value, field_config):
+        """
+        Run all validations for the field
+
+        *Notes*
+
+        * In case choices, validate against choice keys.
+        * Run field validations
+        * Run db checks through
+
+        :return:
+        """
+        check = True
+        # min_length and max_length for string
+        min_length = field_config.get('min_length', None)
+        max_length = field_config.get('min_length', None)
+        validations = field_config.get('validations', None)
+        if min_length:
+            if len(value) < min_length:
+                check = False
+        if max_length:
+            if len(value) > max_length:
+                check = False
+        # field validations
+        if validations:
+            for validation in validations:
+                pass
+        return check
--- ximpia_api/ximpia_api/apps/patterns/__init__.py
+++ ximpia_api/ximpia_api/apps/patterns/__init__.py
@@ -1,1 +1,124 @@
+from document import Document
+from base import exceptions
+
 __author__ = 'jorgealegre'
+
+u"""
+
+We need to define a pattern interface as we advance development on document definition and documents
+
+path:
+document.field
+
+No app definition when we want a pattern
+
+{
+    'customer-code': {
+        'type': 'string',
+        'validations': [
+            {
+                'type': 'not-exists',
+                'path': 'customer.customer-code',
+                'value': 'sjs68ss'
+            }
+        ]
+    }
+}
+
+Data Optimization
+-----------------
+
+How could we send all validations in same query????
+
+We could send bulk query and then parse results!!!
+
+Have a nice way to implement in code
+
+One way is having aa lazy mode, but would be called from the API endpoint, so we only execute once.
+
+This would require that field validate is called from API endpoint and have a different design
+
+"""
+
+
+class NotExists(object):
+
+    data = None
+
+    def __init__(self, data):
+        self.data = data
+
+    def build_query(self):
+        """
+        Build query
+
+        :return:
+        """
+        path = self.data['path']
+        value = self.data['value']
+        path_fields = path.split('.')
+        document = path_fields[0]
+        query = Document.objects.build_bulk_query(
+            document,
+            **{
+                path_fields[1:].replace('.', '__'): value
+            }
+        )
+        return query
+
+    @classmethod
+    def validate(cls, result):
+        """
+        Not exists validation
+
+        This actually is building a bulk query, which gets executed by API endpoint to validate
+        document in one query.
+
+        Based on build_query, validate processes result sent by API endpoint.
+
+        This way we are sure that we only hit index once.
+
+        :return:
+        """
+        if result:
+            return False
+        else:
+            return True
+
+
+class Exists(object):
+
+    data = None
+
+    def __init__(self, data):
+        self.data = data
+
+    def build_query(self):
+        """
+        Build query
+
+        :return:
+        """
+        path = self.data['path']
+        value = self.data['value']
+        path_fields = path.split('.')
+        document = path_fields[0]
+        query = Document.objects.build_bulk_query(
+            document,
+            **{
+                path_fields[1:].replace('.', '__'): value
+            }
+        )
+        return query
+
+    @classmethod
+    def validate(cls, result):
+        """
+        Exists validation
+
+        :return:
+        """
+        if result:
+            return True
+        else:
+            return False
