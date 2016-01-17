38068c3056ba08268099d9949054a7639a718263
Jorge Alegre <git@jorgealegre.com>
2016-01-17 15:16:16 +0100

    document validations and other document changes

--- ximpia_api/ximpia_api/apps/document/tests/test_fields.py
+++ ximpia_api/ximpia_api/apps/document/tests/test_fields.py
@@ -1,1 +1,18 @@
+from django.test import RequestFactory, Client
+
+from base.tests import XimpiaTestCase
+
 __author__ = 'jorgealegre'
+
+
+class StringFieldTest(XimpiaTestCase):
+
+    def setUp(self):
+        self.c = Client()
+        self.req_factory = RequestFactory()
+
+    def tearDown(self):
+        pass
+
+    def test_string(self):
+        pass
